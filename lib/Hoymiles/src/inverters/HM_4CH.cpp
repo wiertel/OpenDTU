a154334fc03ddc0c20f4765a82ef5ae7e2b2379c
Piotr Szydełko <wiertel@wiertel.info>
2022-08-29 16:14:48 +0200

    Accept 1382... serial in Hoymiles HM_4CH

--- lib/Hoymiles/src/inverters/HM_4CH.cpp
+++ lib/Hoymiles/src/inverters/HM_4CH.cpp
@@ -5,12 +5,17 @@ HM_4CH::HM_4CH(uint64_t serial)
 
 bool HM_4CH::isValidSerial(uint64_t serial)
 {
-    return serial >= 0x116100000000 && serial <= 0x116199999999;
+    int16_t prefix = serial >> 32;
+    return (prefix == 0x1161) || (prefix == 0x1382);
 }
 
 String HM_4CH::typeName()
 {
-    return String(F("HM-1200, HM-1500"));
+    if ((this->serial() >> 32) == 0x1382) {
+        return String(F("HM-1800?"));
+    } else {
+        return String(F("HM-1200, HM-1500"));
+    }
 }
 
 const byteAssign_t* HM_4CH::getByteAssignment()
